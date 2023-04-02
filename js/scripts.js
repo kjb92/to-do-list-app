76b5eea8950e9b5d491adc9354fcd3938dc314cc
kjb92 <102806328+kjb92@users.noreply.github.com>
2023-04-02 18:02:17 +0200

    Enable to press 'enter' to add an item

--- js/scripts.js
+++ js/scripts.js
@@ -36,3 +36,16 @@ function newItem() {
     //4. Sorting the list
     $('#list').sortable();
 }
+
+//6. Enable to press 'Enter' to add an item
+$('#input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        $('#button').click();;  
+    }
+});
+
+//7. Prevent page from re-loading on form submission
+$("#form").on("submit", function (e) {
+    e.preventDefault();
+});
